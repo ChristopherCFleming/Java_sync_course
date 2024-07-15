fc2831f3e8f5eee32832047976521695c68ef56a
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2024-07-15 15:41:47 -0400

    Set up boilerplate MYSQL connections

--- pets/src/test/java/data/PetJdbcRepositoryTest.java
+++ pets/src/test/java/data/PetJdbcRepositoryTest.java
@@ -1,7 +1,23 @@
 package data;
 
 import static org.junit.jupiter.api.Assertions.*;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import javax.sql.DataSource;
 
 class PetJdbcRepositoryTest {
+    // 1. Dangerous initialization during construction
+    private DataSource dataSource = initDataSource();
+
+    private DataSource initDataSource() {
+        MysqlDataSource result = new MysqlDataSource();
+        // 2. connection string is:
+        // [db-tech]:[db-vendor]://[host]:[port]/[database-name]
+        result.setUrl("jdbc:mysql://localhost:3306/pets");
+        // 3. username
+        result.setUser("your-username-here");
+        // 4. password
+        result.setPassword("your-password-here");
+        return result;
+    }
 
 }
